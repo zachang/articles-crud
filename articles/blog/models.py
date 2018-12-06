cfe8f46880007eaa23bf2efd8bc3dcb187ac4a4d
zachang <zachangdawuda@gmail.com>
2018-12-06 10:48:46 +0100

    add models to project

---
+++ articles/blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+class AdminProfile(models.Model):  
+    """Represents UserProfile model class"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  
+
+    def __str__(self):  
+          return "{}'s profile".format(self.user,)  
+
+def create_admin_profile(sender, instance, created, **kwargs):  
+    if created:  
+       profile, created = AdminProfile.objects.get_or_create(user=instance)  
+
+post_save.connect(create_admin_profile, sender=User)
+
+class Category(models.Model):
+    """Represents Category model class"""
+
+    category_name = models.CharField(max_length=255)
+
+
+class Article(models.Model):
+    """Represents News model class"""
+
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
